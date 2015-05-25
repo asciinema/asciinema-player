b95ea471833323123bd3841818780574457c72ce
Marcin Kulik <m@ku1ik.com>
2015-05-25 12:19:38 +0200

    Move figwheel out of profiles

--- project.clj
+++ project.clj
@@ -8,14 +8,17 @@
                  [org.clojure/clojurescript "0.0-2511" :scope "provided"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [cljsjs/react "0.13.1-0"]
-                 [reagent "0.5.0"]]
+                 [reagent "0.5.0"]
+                 [figwheel "0.2.1-SNAPSHOT"]
+                 [figwheel-sidecar "0.2.1-SNAPSHOT"]]
 
   :plugins [[lein-cljsbuild "1.0.5"]
+            [lein-figwheel "0.2.1-SNAPSHOT"]
             [lein-less "1.7.2"]]
 
   :min-lein-version "2.5.0"
 
-  :cljsbuild {:builds {:app {:source-paths ["src/cljs"]
+  :cljsbuild {:builds {:dev {:source-paths ["src/cljs"]
                              :figwheel true
                              :compiler {:output-to     "resources/public/js/app.js"
                                         :output-dir    "resources/public/js/out"
@@ -24,6 +27,9 @@
                                         :optimizations :none
                                         :pretty-print  true}}}}
 
+  :figwheel {:http-server-root "public"
+             :server-port 3449
+             :css-dirs ["resources/public/css"]}
 
   :less {:source-paths ["src/less/main"]
          :target-path "resources/public/css"}
@@ -31,17 +37,8 @@
   :profiles {:dev {:source-paths ["env/dev/clj"]
                    :test-paths ["test/clj"]
 
-                   :dependencies [[figwheel "0.2.1-SNAPSHOT"]
-                                  [figwheel-sidecar "0.2.1-SNAPSHOT"]]
-
-                   :plugins [[lein-figwheel "0.2.1-SNAPSHOT"]]
-
-                   :figwheel {:http-server-root "public"
-                              :server-port 3449
-                              :css-dirs ["resources/public/css"]}
-
                    :cljsbuild {:test-commands { "test" ["phantomjs" "env/test/js/unit-test.js" "env/test/unit-test.html"] }
-                               :builds {:app {:source-paths ["env/dev/cljs"]}
+                               :builds {:dev {:source-paths ["env/dev/cljs"]}
                                         :test {:source-paths ["src/cljs" "test/cljs"]
                                                :notify-command ["phantomjs" "env/test/js/unit-test.js" "env/test/unit-test.html"]
                                                :compiler {:output-to     "resources/public/js/app_test.js"
