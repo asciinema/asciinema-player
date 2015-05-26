9c33221d403a29e368b4c90568a1821b0023bc0c
Marcin Kulik <m@ku1ik.com>
2015-05-26 14:21:22 +0200

    Fix lein project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject player "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "asciinema player"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
