87ef1be71a7d87e75210837077d0946daa4d77b4
Marcin Kulik <m@ku1ik.com>
2015-05-26 15:27:19 +0200

    Upgrade to latest ClojureScript

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3211"]
+  :dependencies [[org.clojure/clojure "1.7.0-beta1"]
+                 [org.clojure/clojurescript "0.0-3297"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [cljsjs/react "0.13.1-0"]
                  [reagent "0.5.0"]]
