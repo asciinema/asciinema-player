b3a60bc9b6d522141776d78b92a336dd29fc3494
Marcin Kulik <m@ku1ik.com>
2015-05-29 09:09:08 +0200

    Fix new-position test

--- test/asciinema_player/core_test.cljs
+++ test/asciinema_player/core_test.cljs
@@ -4,7 +4,7 @@
             [asciinema-player.core :as c]))
 
 (deftest new-position-test
-  (is (= (c/new-position 2 5 -3) 0))
-  (is (= (c/new-position 2 5 -1) 1))
-  (is (= (c/new-position 2 5 4) 5))
-  (is (= (c/new-position 2 5 2) 4)))
+  (is (= (c/new-position 2 5 -3) 0.0))
+  (is (= (c/new-position 2 5 -1) 0.2))
+  (is (= (c/new-position 2 5 4) 1.0))
+  (is (= (c/new-position 2 5 2) 0.8)))
