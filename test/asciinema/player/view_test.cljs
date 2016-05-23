fa118dc1d7cabe7878b6b091d8fb9f49ebba3b43
Marcin Kulik <m@ku1ik.com>
2016-05-23 21:15:36 +0200

    Render 24-bit color text

--- test/asciinema/player/view_test.cljs
+++ test/asciinema/player/view_test.cljs
@@ -1,38 +1,39 @@
 (ns asciinema.player.view-test
-  (:require-macros [cljs.test :refer (is deftest testing)])
+  (:require-macros [cljs.test :refer (is are deftest testing)])
   (:require [cljs.test]
             [asciinema.player.view :as v]))
 
-(deftest fg-color-test
-  (is (= (v/fg-color nil false) nil))
-  (is (= (v/fg-color nil true) nil))
-  (is (= (v/fg-color 1 false) 1))
-  (is (= (v/fg-color 1 true) 9))
-  (is (= (v/fg-color 7 true) 15))
-  (is (= (v/fg-color 8 true) 8))
-  (is (= (v/fg-color 15 true) 15)))
+(deftest test-color-class-name
+  (are [color high-intensity class] (= (v/color-class-name color high-intensity "c-") class)
+    nil false nil
+    nil true nil
+    [1 101 201] false nil
+    1 false "c-1"
+    1 true "c-9"
+    7 true "c-15"
+    8 true "c-8"
+    15 true "c-15"))
 
-(deftest bg-color-test
-  (is (= (v/bg-color nil false) nil))
-  (is (= (v/bg-color nil true) nil))
-  (is (= (v/bg-color 1 false) 1))
-  (is (= (v/bg-color 1 true) 9))
-  (is (= (v/bg-color 7 true) 15))
-  (is (= (v/bg-color 8 true) 8))
-  (is (= (v/bg-color 15 true) 15)))
-
-(deftest part-class-name-test
-  (is (= (v/part-class-name {} false) ""))
-  (is (= (v/part-class-name {:fg 1} false) "fg-1"))
-  (is (= (v/part-class-name {:bg 2} false) "bg-2"))
-  (is (= (v/part-class-name {:fg 1 :bold true} false) "fg-9 bright"))
-  (is (= (v/part-class-name {:fg 9 :bold true} false) "fg-9 bright"))
-  (is (= (v/part-class-name {:fg 1 :bg 2 :underline true} false) "fg-1 bg-2 underline"))
-  (is (= (v/part-class-name {:inverse true} false) "fg-bg bg-fg"))
-  (is (= (v/part-class-name {:fg 1 :inverse true} false) "fg-bg bg-1"))
-  (is (= (v/part-class-name {:bg 2 :inverse true} false) "fg-2 bg-fg"))
-  (is (= (v/part-class-name {:fg 1 :bg 2 :inverse true} false) "fg-2 bg-1"))
-  (is (= (v/part-class-name {:fg 1 :bg 2 :bold true :blink true :inverse true} false) "fg-10 bg-9 bright")))
+(deftest test-part-props
+  (are [attrs props] (= (v/part-props attrs) props)
+    {} nil
+    {:fg 1} {:class-name "fg-1"}
+    {:fg [1 2 3]} {:style {:color "rgb(1,2,3)"}}
+    {:bg 2} {:class-name "bg-2"}
+    {:bg [4 5 6]} {:style {:background-color "rgb(4,5,6)"}}
+    {:fg 1 :bold true} {:class-name "fg-9 bright"}
+    {:fg 9 :bold true} {:class-name "fg-9 bright"}
+    {:fg 1 :bg 2 :underline true} {:class-name "fg-1 bg-2 underline"}
+    {:fg [1 2 3] :bg [4 5 6] :bold true :underline true} {:class-name "bright underline" :style {:color "rgb(1,2,3)" :background-color "rgb(4,5,6)"}}
+    ;; inversed colors
+    {:inverse true} {:class-name "fg-bg bg-fg"}
+    {:inverse true :fg 1} {:class-name "fg-bg bg-1"}
+    {:inverse true :fg [1 2 3]} {:class-name "fg-bg" :style {:background-color "rgb(1,2,3)"}}
+    {:inverse true :bg 2} {:class-name "fg-2 bg-fg"}
+    {:inverse true :bg [4 5 6]} {:class-name "bg-fg" :style {:color "rgb(4,5,6)"}}
+    {:inverse true :fg 1 :bg 2} {:class-name "fg-2 bg-1"}
+    {:inverse true :fg 1 :bg 2 :bold true :blink true} {:class-name "fg-10 bg-9 bright"}
+    {:inverse true :fg [1 2 3] :bg [4 5 6]} {:style {:color "rgb(4,5,6)" :background-color "rgb(1,2,3)"}}))
 
 (deftest elapsed-time-test
   (is (= (v/elapsed-time 0.88) "00:00"))
