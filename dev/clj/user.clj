423903f937dd49c0d050937373cbc0cdabfeb8c0
Marcin Kulik <m@ku1ik.com>
2017-01-28 12:23:59 +0100

    Add helpers to dev ns

--- dev/clj/user.clj
+++ dev/clj/user.clj
@@ -9,3 +9,9 @@
 (defn stop [] (ra/stop-figwheel!))
 
 (defn cljs [] (ra/cljs-repl "dev"))
+
+(comment
+  (let [codes [27 91 49 74]]
+    #_(map #(format "%x" %) codes)
+    (String. (int-array codes) 0 (count codes)))
+)
