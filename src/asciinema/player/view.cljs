d7b20f130c53f73e9abac180c48ed303e021a41b
Marcin Kulik <m@ku1ik.com>
2017-02-16 11:34:24 +0100

    Fix poster rendering

--- src/asciinema/player/view.cljs
+++ src/asciinema/player/view.cljs
@@ -119,9 +119,9 @@
   (let [class-name (reaction (terminal-class-name @font-size))
         style (reaction (terminal-style @width @height @font-size))
         lines (reaction (screen/lines @screen))
-        line-reactions (mapv (fn [n]
-                               (reaction (get @lines n)))
-                             (range (count @lines)))
+        line-reactions (reaction (mapv (fn [n]
+                                         (reaction (get @lines n)))
+                                       (range @height)))
         cursor (reaction (screen/cursor @screen))
         cursor-x (reaction (:x @cursor))
         cursor-y (reaction (:y @cursor))
@@ -134,7 +134,7 @@
                                                     (= idx @cursor-y)
                                                     @cursor-x))]
                         ^{:key idx} [line parts cursor-x cursor-on]))
-                    line-reactions)])))
+                    @line-reactions)])))
 
 (def logo-raw-svg "<defs> <mask id=\"small-triangle-mask\"> <rect width=\"100%\" height=\"100%\" fill=\"white\"/> <polygon points=\"508.01270189221935 433.01270189221935, 208.0127018922194 259.8076211353316, 208.01270189221927 606.217782649107\" fill=\"black\"></polygon> </mask> </defs> <polygon points=\"808.0127018922194 433.01270189221935, 58.01270189221947 -1.1368683772161603e-13, 58.01270189221913 866.0254037844386\" mask=\"url(#small-triangle-mask)\" fill=\"white\"></polygon> <polyline points=\"481.2177826491071 333.0127018922194, 134.80762113533166 533.0127018922194\" stroke=\"white\" stroke-width=\"90\"></polyline>")
 
