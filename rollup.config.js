32c1f4ae6b31ea509d352f19b4298c9e10aafd5f
Marcin Kulik <m@ku1ik.com>
2021-05-28 22:26:29 +0200

    Minimize with terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
 import rust from "@wasm-tool/rollup-plugin-rust";
+import { terser } from "rollup-plugin-terser";
 
 const plugins = [
   babel({
@@ -14,10 +15,17 @@ const plugins = [
 
 export default {
   input: "src/index.js",
-  output: {
-    file: "public/bundle.js",
-    format: "iife"
-  },
+  output: [
+    {
+      file: "public/bundle.js",
+      format: "iife"
+    },
+    {
+      file: "public/bundle.min.js",
+      format: "iife",
+      plugins: [terser()]
+    }
+  ],
   preserveEntrySignatures: false,
   plugins
 };
