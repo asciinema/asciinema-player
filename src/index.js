61bd1dd23611c3f250ee4cb85d2a845df5166135
Marcin Kulik <m@ku1ik.com>
2021-05-30 17:29:00 +0200

    Pass all createAsciinemaPlayer opts as main component props

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { render } from 'solid-js/web';
 import AsciinemaPlayer from './components/AsciinemaPlayer';
 
-window.createAsciinemaPlayer = (url, opts, elem) => {
-  render(() => (<AsciinemaPlayer src={url} size={opts.size} />), elem);
+if (window) {
+  window.createAsciinemaPlayer = (props, elem) => {
+    render(() => (<AsciinemaPlayer {...props} />), elem);
+  }
 }
