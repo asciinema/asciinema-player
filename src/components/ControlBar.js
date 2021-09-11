3535a7aac3c31a641041d4c2bb261cbae21ba487
Marcin Kulik <m@ku1ik.com>
2021-09-11 19:01:16 +0200

    Enable tooltip for fullscreen button

--- src/components/ControlBar.js
+++ src/components/ControlBar.js
@@ -69,7 +69,7 @@ export default props => {
         <span class="time-remaining">{remainingTime()}</span>
       </span>
 
-      <span class="fullscreen-button" onClick={e(props.onFullscreenClick)}>
+      <span class="fullscreen-button" onClick={e(props.onFullscreenClick)} title="Toggle fullscreen mode">
         <svg version="1.1" viewBox="0 0 12 12" class="icon">
           <path d="M12,0 L7,0 L9,2 L7,4 L8,5 L10,3 L12,5 Z"></path>
           <path d="M0,12 L0,7 L2,9 L4,7 L5,8 L3,10 L5,12 Z"></path>
