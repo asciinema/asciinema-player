28a301b60f734f28fa004056fb990609083d2269
Marcin Kulik <m@ku1ik.com>
2021-09-11 18:43:15 +0200

    lesscss mixin for overriding big play button color

--- src/components/StartOverlay.js
+++ src/components/StartOverlay.js
@@ -13,8 +13,8 @@ export default props => {
                   <polygon points="508.01270189221935 433.01270189221935, 208.0127018922194 259.8076211353316, 208.01270189221927 606.217782649107" fill="black"></polygon>
                 </mask>
               </defs>
-              <polygon points="808.0127018922194 433.01270189221935, 58.01270189221947 -1.1368683772161603e-13, 58.01270189221913 866.0254037844386" mask="url(#small-triangle-mask)" fill="white"></polygon>
-              <polyline points="481.2177826491071 333.0127018922194, 134.80762113533166 533.0127018922194" stroke="white" stroke-width="90"></polyline>
+              <polygon points="808.0127018922194 433.01270189221935, 58.01270189221947 -1.1368683772161603e-13, 58.01270189221913 866.0254037844386" mask="url(#small-triangle-mask)" fill="white" class="play-btn-fill"></polygon>
+              <polyline points="481.2177826491071 333.0127018922194, 134.80762113533166 533.0127018922194" stroke="white" stroke-width="90" class="play-btn-stroke"></polyline>
             </svg>
           </span>
         </div>
