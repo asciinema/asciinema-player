1a9f91c752cd4a9ff4f21cb2cffbc5bf3a5ce0b6
Marcin Kulik <m@ku1ik.com>
2021-09-18 15:19:51 +0200

    Expose player's DOM element in return value of create(...)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ import { render } from 'solid-js/web';
 import AsciinemaPlayer from './components/AsciinemaPlayer';
 
 function create(props, elem) {
-  return render(() => (<AsciinemaPlayer {...props} />), elem);
+  let el;
+
+  const dispose = render(() => {
+    el = <AsciinemaPlayer {...props} />;
+    return el;
+  }, elem);
+
+  return {
+    el: el,
+    dispose: dispose
+  }
 }
 
 export { create };
