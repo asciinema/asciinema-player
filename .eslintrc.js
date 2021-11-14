f5fb228a24cff7f0913a4cdd60c132338297d411
Marcin Kulik <m@ku1ik.com>
2021-11-14 15:36:19 +0100

    Remove unused packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    "extends": ["eslint:recommended", "@solid/eslint-config-base"],
+    "extends": "eslint:recommended",
     "parserOptions": {
         "ecmaVersion": 12,
         "sourceType": "module"
