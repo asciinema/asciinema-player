db2c3769d1ec29d0de8ccb6ed89f4c054c2c949b
Marcin Kulik <m@ku1ik.com>
2021-11-17 21:53:26 +0100

    Upgrade src arg to object automatically

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import Player from './components/Player';
 function create(props, elem) {
   let el;
 
+  if (typeof props === 'string') {
+    props = { src: props };
+  }
+
   const dispose = render(() => {
     el = <Player {...props} />;
     return el;
