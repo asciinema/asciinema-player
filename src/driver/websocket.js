b056642ff76757bcb3b9481670c2f5e3b34397f7
Marcin Kulik <m@ku1ik.com>
2021-11-23 22:50:16 +0100

    Implement reconnection with backoff for WebSocket driver

--- src/driver/websocket.js
+++ src/driver/websocket.js
@@ -3,23 +3,42 @@ import buffer from '../buffer';
 function websocket({ url, bufferTime = 0 }, { feed }) {
   const buf = buffer(feed, bufferTime);
   let socket;
+  let reconnectDelay = 250;
+
+  function connect() {
+    socket = new WebSocket(url);
+    socket.binaryType = 'arraybuffer';
+
+    socket.onopen = () => {
+      reconnectDelay = 250;
+    }
+
+    socket.onmessage = event => {
+      if (typeof event.data === 'string') {
+        buf.pushEvent(JSON.parse(event.data));
+      } else {
+        buf.pushText(String.fromCharCode.apply(null, new Uint8Array(event.data)));
+      }
+    }
+
+    socket.onclose = event => {
+      if (!event.wasClean) {
+        console.debug(`reconnecting in ${reconnectDelay}...`);
+        setTimeout(connect, reconnectDelay);
+        reconnectDelay = Math.min(reconnectDelay * 2, 5000);
+      }
+    }
+  }
 
   return {
     start: () => {
-      socket = new WebSocket(url);
-      socket.binaryType = 'arraybuffer';
-
-      socket.onmessage = event => {
-        if (typeof event.data === 'string') {
-          buf.pushEvent(JSON.parse(event.data));
-        } else {
-          buf.pushText(String.fromCharCode.apply(null, new Uint8Array(event.data)));
-        }
-      }
+      connect();
     },
 
     stop: () => {
-      socket.close();
+      if (socket !== undefined) {
+        socket.close();
+      }
     }
   }
 }
