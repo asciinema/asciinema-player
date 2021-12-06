17f2a3841e20ea0ba06fd410ae2e0eb40bc29c59
Marcin Kulik <m@ku1ik.com>
2021-12-06 23:01:03 +0100

    Reset VT in websocket/eventsource driver when receiving new terminal dimensions

--- src/driver/eventsource.js
+++ src/driver/eventsource.js
@@ -1,28 +1,43 @@
 import buffer from '../buffer';
 
-function eventsource({ url, bufferTime = 0 }, { feed }) {
-  const buf = buffer(feed, bufferTime);
+function eventsource({ url, bufferTime = 0 }, { feed, reset }) {
   let es;
+  let buf;
+
+  function initBuffer() {
+    if (buf !== undefined) buf.stop();
+    buf = buffer(feed, bufferTime);
+  }
 
   return {
     start: () => {
       es = new EventSource(url);
 
+      es.addEventListener('open', () => {
+        console.debug('eventsource: opened');
+        initBuffer();
+      });
+
       es.addEventListener('message', event => {
-        buf.pushEvent(JSON.parse(event.data));
+        const e = JSON.parse(event.data);
+
+        if (e.cols !== undefined || e.width !== undefined) {
+          initBuffer();
+          reset(e.cols ?? e.width, e.rows ?? e.height);
+        } else {
+          buf.pushEvent(e);
+        }
       });
 
       es.addEventListener('done', () => {
+        console.debug('eventsource: closed');
         es.close();
       });
     },
 
     stop: () => {
-      buf.stop();
-
-      if (es !== undefined) {
-        es.close();
-      }
+      if (buf !== undefined) buf.stop();
+      if (es !== undefined) es.close();
     }
   }
 }
--- src/driver/websocket.js
+++ src/driver/websocket.js
@@ -1,35 +1,50 @@
 import buffer from '../buffer';
 
-function websocket({ url, bufferTime = 0 }, { feed }) {
-  const buf = buffer(feed, bufferTime);
+function websocket({ url, bufferTime = 0 }, { feed, reset }) {
   const utfDecoder = new TextDecoder();
   let socket;
+  let buf;
   let reconnectDelay = 250;
   let stop = false;
+
+  function initBuffer() {
+    if (buf !== undefined) buf.stop();
+    buf = buffer(feed, bufferTime);
+  }
 
   function connect() {
     socket = new WebSocket(url);
     socket.binaryType = 'arraybuffer';
 
     socket.onopen = () => {
-      console.debug(`websocket: connected`);
+      console.debug('websocket: opened');
+      initBuffer();
       reconnectDelay = 250;
     }
 
     socket.onmessage = event => {
       if (typeof event.data === 'string') {
-        buf.pushEvent(JSON.parse(event.data));
+        const e = JSON.parse(event.data);
+
+        if (e.cols !== undefined || e.width !== undefined) {
+          initBuffer();
+          reset(e.cols ?? e.width, e.rows ?? e.height);
+        } else {
+          buf.pushEvent(e);
+        }
       } else {
         buf.pushText(utfDecoder.decode(event.data));
       }
     }
 
     socket.onclose = event => {
-      if (stop || event.wasClean) return;
-
-      console.debug(`websocket: unclean close, reconnecting in ${reconnectDelay}...`);
-      setTimeout(connect, reconnectDelay);
-      reconnectDelay = Math.min(reconnectDelay * 2, 5000);
+      if (stop || event.wasClean) {
+        console.debug('websocket: closed');
+      } else {
+        console.debug(`websocket: unclean close, reconnecting in ${reconnectDelay}...`);
+        setTimeout(connect, reconnectDelay);
+        reconnectDelay = Math.min(reconnectDelay * 2, 5000);
+      }
     }
   }
 
@@ -40,11 +55,8 @@ function websocket({ url, bufferTime = 0 }, { feed }) {
 
     stop: () => {
       stop = true;
-      buf.stop();
-
-      if (socket !== undefined) {
-        socket.close();
-      }
+      if (buf !== undefined) buf.stop();
+      if (socket !== undefined) socket.close();
     }
   }
 }
