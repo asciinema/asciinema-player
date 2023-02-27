f7c5a486adf3e17723bba9099f516910599140f0
Marcin Kulik <m@ku1ik.com>
2023-02-27 09:59:56 +0100

    Simplify queueing and feeding in high freq live streams

--- src/queue.js
+++ src/queue.js
@@ -1,67 +1,29 @@
 class Queue {
   constructor() {
-    this.first = undefined;
-    this.last = undefined;
+    this.items = [];
     this.onPush = undefined;
   }
 
   push(item) {
-    const node = { item: item };
+    this.items.push(item);
 
-    if (this.last !== undefined) {
-      this.last = this.last.next = node;
-    } else {
-      this.last = this.first = node;
-    }
-
-    if (this.onPush) {
-      this.onPush(this.pop());
+    if (this.onPush !== undefined) {
+      this.onPush(this.popAll());
       this.onPush = undefined;
     }
   }
 
-  pop() {
-    const node = this.first;
-
-    if (node !== undefined) {
-      this.first = node.next;
-
-      if (this.first === undefined) {
-        this.last = undefined;
-      }
-
-      return node.item;
+  popAll() {
+    if (this.items.length > 0) {
+      const items = this.items;
+      this.items = [];
+      return items;
     } else {
       const thiz = this;
 
       return new Promise(resolve => {
         thiz.onPush = resolve;
       });
-    }
-  }
-
-  forEach(f) {
-    let stop = false;
-
-    const go = async () => {
-      let item = this.pop();
-
-      while (typeof item !== 'object' || typeof item.then !== 'function') {
-        if (stop) return;
-        await f(item);
-        item = this.pop();
-      }
-
-      item = await item;
-      if (stop) return;
-      await f(item);
-      go();
-    }
-
-    setTimeout(go, 0);
-
-    return () => {
-      stop = true;
     }
   }
 }
