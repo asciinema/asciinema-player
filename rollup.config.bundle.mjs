ad5dd16efd467785610d2c53e591e96be8f56ef6
Marcin Kulik <m@ku1ik.com>
2023-03-02 16:35:26 +0100

    Upgrade to Rollup v3

--- rollup.config.bundle.mjs
+++ rollup.config.bundle.mjs
@@ -1,6 +1,6 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 
 export default {
   input: "dist/index.js",
