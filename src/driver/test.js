9ce043f461afdac7cba974eeb8a54aebee6e1d83
Marcin Kulik <m@ku1ik.com>
2023-04-02 08:57:26 +0200

    Benchmark through test driver
    AsciinemaPlayer.create({
      driver: 'test',
      kind: 'benchmark',
      url: '/demo.cast',
      iterations: 100,
    }, document.body, { preload: true });

--- src/driver/test.js
+++ src/driver/test.js
@@ -1,8 +1,13 @@
-function test({ kind }, callbacks, opts) {
-  if (kind == 'random') {
+import { parseAsciicast } from '../parser/asciicast';
+
+
+function test(src, callbacks, opts) {
+  if (src.kind == 'random') {
     return random(callbacks);
-  } else if (kind == 'clock') {
+  } else if (src.kind == 'clock') {
     return clock(callbacks, opts);
+  } else if (src.kind == 'benchmark') {
+    return benchmark(src, callbacks, opts);
   }
 }
 
@@ -72,6 +77,44 @@ function clock({ feed }, { cols = 5, rows = 1 }) {
       return d.getHours() * 60 + d.getMinutes();
     }
   };
+}
+
+function benchmark({ url, iterations = 10 }, { feed, now }) {
+  let frames;
+  let byteCount = 0;
+
+  return {
+    async init() {
+      const asciicast = parseAsciicast(await (await fetch(url)).text());
+      const { cols, rows } = asciicast;
+      frames = Array.from(asciicast.frames);
+      const duration = frames[frames.length - 1][0];
+
+      for (const [_, text] of frames) {
+        byteCount += new Blob([text]).size;
+      }
+
+      return { cols, rows, duration };
+    },
+
+    play() {
+      const startTime = now();
+
+      for (let i = 0; i < iterations; i++) {
+        for (const [_, text] of frames) {
+          feed(text);
+        }
+        feed('\x1bc'); // reset terminal
+    }
+
+      const endTime = now();
+      const duration = (endTime - startTime) / 1000;
+      const throughput = (byteCount * iterations) / duration;
+      const throughputMbs = (byteCount / (1024 * 1024) * iterations) / duration;
+
+      console.info('benchmark: result', { byteCount, iterations, duration, throughput, throughputMbs });
+    }
+  }
 }
 
 export { test };
