87c9d041be1efd313c795e19bf734e7f4b08f304
Marcin Kulik <m@ku1ik.com>
2023-05-25 15:19:39 +0200

    Reset LZW dict upon reset code in input

--- src/lzw.js
+++ src/lzw.js
@@ -1,4 +1,5 @@
 const MAX_DICT_SIZE = 4096;
+const RESET_CODE = 256;
 
 export default class LzwDecompressor {
   constructor() {
@@ -7,6 +8,7 @@ export default class LzwDecompressor {
 
   resetDictionary() {
     this.dictionary = new Map();
+    this.nextCode = RESET_CODE + 1;
 
     for (let b = 0; b < 256; b++) {
       this.dictionary.set(b, [b]);
@@ -14,41 +16,47 @@ export default class LzwDecompressor {
   }
 
   decompress(input) {
-    const code = input.getUint16(0, true);
-    let lastSeq = this.dictionary.get(code);
-    let seqs = [lastSeq];
-    let outputLength = lastSeq.length;
-    let inputOffset = 2;
+    let inputOffset = 0;
+    let outputLength = 0;
+    let seqs = [];
+    let lastSeq = [];
 
     while (inputOffset < input.byteLength) {
       const code = input.getUint16(inputOffset, true);
+      inputOffset += 2;
+
+      if (code === RESET_CODE) {
+        this.resetDictionary();
+        lastSeq = [];
+        continue;
+      }
+
       let seq = this.dictionary.get(code);
 
       if (seq !== undefined) {
         seqs.push(seq);
         outputLength += seq.length;
 
-        if (this.dictionary.size < MAX_DICT_SIZE) {
-            this.dictionary.set(this.dictionary.size, lastSeq.concat([seq[0]]));
-        } else {
-            this.resetDictionary();
+        if (this.nextCode < MAX_DICT_SIZE && lastSeq.length > 0) {
+          this.dictionary.set(this.nextCode, lastSeq.concat([seq[0]]));
+          this.nextCode++;
         }
-      } else if (code == this.dictionary.size) {
-        seq = lastSeq.concat([lastSeq[0]])
-        seqs.push(seq);
-        outputLength += seq.length;
+      } else if (code == this.nextCode) {
+        if (lastSeq.length > 0) {
+          seq = lastSeq.concat([lastSeq[0]])
+          seqs.push(seq);
+          outputLength += seq.length;
 
-        if (this.dictionary.size < MAX_DICT_SIZE) {
-            this.dictionary.set(this.dictionary.size, seq);
-        } else {
-            this.resetDictionary();
+          if (this.nextCode < MAX_DICT_SIZE) {
+            this.dictionary.set(this.nextCode, seq);
+            this.nextCode++;
+          }
         }
       } else {
         throw `invalid code ${code} (dict size: ${this.dictionary.size})`;
       }
 
       lastSeq = seq;
-      inputOffset += 2;
     }
 
     const output = new Uint8Array(new ArrayBuffer(outputLength));
