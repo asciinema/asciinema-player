cad9ea2520de2a3f44ff4f57235d0a9d09b12f72
Marcin Kulik <m@ku1ik.com>
2024-02-13 11:23:46 +0100

    Remove wee_alloc dep - it was not enabled as the global allocator anyway

--- src/vt/src/lib.rs
+++ src/vt/src/lib.rs
@@ -1,11 +1,6 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 use avt::Vt;
 
