30aed0472172b3cd38ff5cc50201461858229d5a
Marcin Kulik <m@ku1ik.com>
2026-03-30 18:05:40 +0200

    Remove redundant terminal size vars

--- src/core.js
+++ src/core.js
@@ -291,8 +291,6 @@ class Core {
 
   _resetVt(cols, rows, init = undefined, theme = undefined) {
     this.logger.debug(`core: vt reset (${cols}x${rows})`);
-    this.cols = cols;
-    this.rows = rows;
     this._initializeVt(cols, rows);
 
     if (init !== undefined && init !== "") {
@@ -307,11 +305,11 @@ class Core {
   }
 
   _resizeVt(cols, rows) {
-    if (cols === this.vt.cols && rows === this.vt.rows) return;
+    if (cols === this.cols && rows === this.rows) return;
 
     const changedRows = this.vt.resize(cols, rows);
-    this.vt.cols = cols;
-    this.vt.rows = rows;
+    this.cols = cols;
+    this.rows = rows;
     this.logger.debug(`core: vt resize (${cols}x${rows})`);
 
     this._dispatchEvent("vtUpdate", {
@@ -323,8 +321,8 @@ class Core {
   _initializeVt(cols, rows) {
     this.logger.debug('vt init', { cols, rows });
     this.vt = this.wasm.create(cols, rows, 100, this.boldIsBright);
-    this.vt.cols = cols;
-    this.vt.rows = rows;
+    this.cols = cols;
+    this.rows = rows;
   }
 
   _parsePoster(poster) {
